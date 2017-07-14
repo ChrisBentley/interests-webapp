6bd721caab04f7cc02fef861147068b8c2298b5e
ChrisBentley <c.j.bentley90@gmail.com>
2017-07-14 17:05:10 +0100

    Added first test for /help handler

--- API-server/src/routes/help/handlers.test.js
+++ API-server/src/routes/help/handlers.test.js
@@ -1,1 +1,26 @@
-console.log('help handler test not implemented yet');
+const helpHandler = require('./handlers.js');
+
+var MockExpressRequest = require('mock-express-request');
+var MockExpressResponse = require('mock-express-response');
+
+let request = new MockExpressRequest;
+let response = new MockExpressResponse;
+let next = function() {};
+
+describe('Testing help route handlers', () => {
+
+    beforeEach(() => {
+        // console.log('beforeEach ran');
+    });
+
+    afterEach(() => {
+        // console.log('afterEach ran');
+    });
+
+    it('should return a 200 and the help page text', () => {
+        helpHandler.help(request, response, next);
+
+        expect(response.statusCode).to.equal(200);
+        expect(response._getString()).to.equal('This is the help html page');
+    });
+});
--- API-server/unitTestConfig.js
+++ API-server/unitTestConfig.js
@@ -6,4 +6,10 @@ const doc = win.window;
 global.window = win;
 global.doc = doc;
 
+const chai = require('chai');
+let sinonChai = require('sinon-chai');
+chai.use(require('chai-as-promised'));
+chai.use(sinonChai);
+
+global.expect = chai.expect;
 global.sinon = require('sinon');
