a2c46222f7225f46a0e1515b983b875c7d096ac3
ChrisBentley <c.j.bentley90@gmail.com>
2017-07-18 14:34:18 +0100

    completed scraper for books

--- API-server/src/components/data-scraper/scraper.js
+++ API-server/src/components/data-scraper/scraper.js
@@ -2,20 +2,40 @@ const cheerio = require('cheerio');
 
 var request = require('request');
 
-request('http://www.goodreads.com/review/list/52192894?shelf=currently-reading', function(error, response, html) {
+var requestUrl = 'https://www.goodreads.com/review/list/52192894?shelf=currently-reading';
+
+request(requestUrl, function(error, response, html) {
   if (!error && response.statusCode == 200){
-    // console.log(html);
+
     var $ = cheerio.load(html);
 
     const booksElements = $('#booksBody').children();
 
-    var bookTitles = [];
+    var books = [];
 
     for(var i=0; i < booksElements.length; i++) {
       $ = cheerio.load(booksElements[i]);
-      bookTitles.push($('.title .value').text().trim());
+      var bookDetails = {};
+
+      bookDetails.title = $('.title .value').text().trim();
+
+      var smallImgUrl = $('.cover .value div div a img').attr('src').split('s/');
+      bookDetails.imgUrl = smallImgUrl[0] + 's/' + smallImgUrl[1] + 'l/' + smallImgUrl[2];
+
+      var author = $('.author .value a').text().trim().split(', ');
+      bookDetails.author = author[1] + ' ' + author[0];
+
+      bookDetails.avgRating = $('.avg_rating .value').text().trim();
+
+      bookDetails.myRating = $('.shelves .value .stars').attr('data-rating');
+
+      bookDetails.dateAdded = $('.date_added .value span').text().trim();
+
+      bookDetails.dateCompleted = $('.date_read .value span').text().trim();
+
+      books.push(bookDetails);
     }
 
-    console.log(bookTitles);
+    console.log(books);
   }
 });
